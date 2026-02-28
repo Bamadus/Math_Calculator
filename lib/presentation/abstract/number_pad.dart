e50122f7bf718cf1e72a5108a483f72436332b12
Bamadus <olamidotunbamiloye@gmail.com>
2026-02-28 19:35:27 +0100

    fist commit

---
+++ lib/presentation/abstract/number_pad.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class Number_pad extends StatelessWidget{
+  final String? number;
+  final Color? color;
+  final double? size;
+
+  Number_pad({
+    super.key, 
+    this.number,
+    this.color,
+    this.size,
+    });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all( 10),
+      // padding: EdgeInsets.all(35.0),
+      decoration: ShapeDecoration(
+        color: const Color(0xffdee2e6),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(100.0),
+        ),
+      ),
+      // color: Colors.red,
+      child: Center(
+        child: Text('${number ?? 0}',
+        style: TextStyle(
+          fontSize: size ?? 25,
+          fontFamily: 'Roboto',
+          fontWeight: FontWeight.w400,
+          color: color ?? Colors.white,
+        ),
+            ),
+      )
+    );
+  }
+}
---
+++ lib/presentation/home.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'abstract/number_pad.dart';
+
+class CalcScreen extends StatefulWidget {
+  const CalcScreen(Function? flipTheme ,{super.key});
+
+  @override
+  State<CalcScreen> createState() => _CalcScreenState();
+}
+
+class _CalcScreenState extends State<CalcScreen> {
+
+  dynamic get flipTheme => flipTheme;
+  
+  double screenHeight(BuildContext context) => MediaQuery.of(context).size.height;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 150, 197, 220),
+        title: const Text('BAMADUS'),
+        leading: IconButton(onPressed:flipTheme, 
+        color: Colors.black54,
+        icon: Image.asset("assets/black_brush.png",)
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.settings, color: Colors.black45, size: 30),
+          ),
+
+        ],
+      ),
+      body: Column(
+        children:[
+          SizedBox(
+            height: screenHeight(context)*0.25,
+          ),
+          Divider(
+            thickness: 2,
+          ),
+          SizedBox(),
+          OrientationBuilder(
+            builder: (context, orientation) {
+              return GridView.count(
+            crossAxisSpacing: 10,
+            // crossAxisCount: 4,
+            shrinkWrap: true,
+            crossAxisCount:
+                  orientation == Orientation.portrait ? 4 : 5,
+                  mainAxisSpacing: orientation == Orientation.landscape ? 5 : 4,
+            children: [
+              Number_pad(number: 'C', color: const Color(0xffd00000)),
+              Number_pad(number: '( )', color: const Color(0xff006400)),
+              Number_pad(number: '%', color: const Color(0xff006400)),
+              Number_pad(number: '÷', color: const Color(0xff006400)),
+              Number_pad(number: '7', color: const Color(0xff212529)),
+              Number_pad(number: '8', color: const Color(0xff212529),),
+              Number_pad(number: '9', color: const Color(0xff212529)),
+              Number_pad(number: 'x', color: const Color(0xff006400)),
+              Number_pad(number: '4', color: const Color(0xff212529),),
+              Number_pad(number: '5', color: const Color(0xff212529),),
+              Number_pad(number: '6', color: const Color(0xff212529),),
+              Number_pad(number: '-', size: 30, color: const Color(0xff006400)),
+              Number_pad(number: '1', color: const Color(0xff212529),),
+              Number_pad(number: '2', color: const Color(0xff212529),),
+              Number_pad(number: '3', color: const Color(0xff212529),),
+              Number_pad(number: '+', color: const Color(0xff006400)),
+
+              // Container(
+              //   color: const Color(0xffdee2e6),
+              //   child: IconButton(
+              //     onPressed: () {},
+              //     icon: Icon(Icons.settings, color: Colors.black12, size: 30),
+              //   ),
+              // ),
+
+              Number_pad(number: '0', color: const Color(0xff212529),),
+              Number_pad(number: '.', color: const Color(0xff212529),),
+              Number_pad(number: '=', color: const Color(0xff006400),),
+            ],
+          );
+            }
+            )
+        
+        ]
+      )
+    );
+  }
+}
